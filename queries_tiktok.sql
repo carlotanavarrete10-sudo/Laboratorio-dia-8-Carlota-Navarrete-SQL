f7bd994c5123f146a6decd6db91e1f40e6cb45fd
carlotanavarrete10-sudo <carlotanavarrete10@gmail.com>
2025-09-19 13:58:51 +0200

    Add files via upload

---
+++ queries_tiktok.sql
@@ -0,0 +1,26 @@
+-- consulta datos 
+
+-- ver todos los usuarios de tiktok
+SELECT * FROM usuarios; 
+
+-- ver todos los videos publicados.
+SELECT * FROM videos;
+
+-- Ver los comentarios realizados en los videos.
+SELECT * FROM comentarios; 
+
+-- Ver todos los likes dados a los videos.
+SELECT * FROM likes;
+
+-- Ver las relaciones de seguimiento entre los usuarios.
+SELECT * FROM seguidores;
+
+-- Query 1 
+SELECT email FROM usuarios; 
+
+-- Query 2
+SELECT id_video FROM comentarios;
+
+-- Query 3
+SELECT fecha_like FROM likes; 
+
